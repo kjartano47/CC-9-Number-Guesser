ce1b2489ee4310153fc327091956751d55104c30
kjartano47 <kjartano97@gmail.com>
2022-11-12 17:40:38 +0000

    added score updater and guess comparer

--- script.js
+++ script.js
@@ -4,3 +4,25 @@ let currentRoundNumber = 1;
 
 // Write your code below:
 
+const generateTarget = () => {
+	let random = Math.floor(Math.random() * 10);
+	return random;
+};
+
+const compareGuesses = (human, computer, target) => {
+	let hCloseness = Math.abs(human - target);
+	let cCloseness = Math.abs(computer - target);
+	if (hCloseness <= cCloseness) {
+		return true;
+	} else {
+		return false;
+	}
+};
+
+const updateScore = (winner) => {
+	if (winner === "human") {
+		humanScore++;
+	} else if (winner === "computer") {
+		computerScore++;
+	}
+};
